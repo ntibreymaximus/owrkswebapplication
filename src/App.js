784e5d31268e8d168836b1c7cc8fe9553ec9be13
maximusntibrey <maximuslambertntibrey.mln@gmail.com>
2021-09-27 16:08:58 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,45 @@ import Routes from "./components/routes";
 export default function App() {
 	return (
 		<div className='App'>
-			<Routes />
+			<div className='login'>
+				<h1 className='login-header'>OWRKS</h1>
+				<div className='col-md-6'>
+					<form>
+						<h2 className='loginform-header'>Admin Login</h2>
+						<div className='form-group'>
+							<label htmlFor='username'>Username</label>
+							<div className='forminputcontainer'>
+								<input
+									type='text'
+									name='username'
+									className='form-control forminput'
+									id='username'
+									aria-describedby='username'
+								/>
+							</div>
+							<label htmlFor='password'>Password</label>
+							<div className='forminputcontainer'>
+								<input
+									type='password'
+									name='password'
+									className='form-control forminput'
+									id='password'
+								/>
+							</div>
+							<div className='forminputcontainer'>
+								<Link to='/dashboard/home'>
+									<button type='button' className='btn btn-primary formbutton'>
+										Login
+									</button>
+								</Link>
+							</div>
+						</div>
+					</form>
+				</div>
+				<Route path='/'>
+					<Dashboard />
+				</Route>
+			</div>
 		</div>
 	);
 }
