fadbfb048ec13a72419ec1783b5d0a4b0479ac42
Mighty Man <donhollyodonkor@gmail.com>
2021-10-04 12:24:46 +0000

    Revert "main dashboard ui update"
    This reverts commit 219e03327ffdf2a3cf1fe24a990d985df9f75ec7.

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,46 +1,19 @@
 import React, { Component } from "react";
-import "../css/sidenavigation.css";
-import { Nav, Container, Sidebar, Sidenav, Button } from "rsuite";
+import { Col, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "../css/dashboard.css";
+import "../css/sidenavigation.css";
+import SideNavigation from "./sidenavigation";
 export default class Dashboard extends Component {
   render() {
     return (
       <Container className="container">
-        <div className="navbar">
-          <Sidebar className="sidenavigation">
-            <Sidenav>
-              <Sidenav.Header className="sidebarheader">
-                <h3 className="sidebarheaderh3">Welcome to</h3>
-                <h1 className="sidebarheaderh1">OWRKS</h1>
-              </Sidenav.Header>
-              <Sidenav.Body className="sidenavigationbody">
-                <Nav>
-                  <Link to="/dashboard" className="navlink">
-                    <Nav.Item className="navitem navitemactive">
-                      Dashboard
-                    </Nav.Item>
-                  </Link>
-                  <Link to="/users" className="navlink">
-                    <Nav.Item className="navitem">Users</Nav.Item>
-                  </Link>
-                  <Link to="/products" className="navlink">
-                    <Nav.Item className="navitem">Products</Nav.Item>
-                  </Link>
-                  <Link to="/suppliers" className="navlink">
-                    <Nav.Item className="navitem">Suppliers</Nav.Item>
-                  </Link>
-                  <Link to="/transactions" className="navlink">
-                    <Nav.Item className="navitem">Transactions</Nav.Item>
-                  </Link>
-                  <Link to="/">
-                    <Button className="logoutbutton">Logout</Button>
-                  </Link>
-                </Nav>
-              </Sidenav.Body>
-            </Sidenav>
-          </Sidebar>
-        </div>
-        <div className="navcontent"></div>
+        <Col md={3}>
+          <SideNavigation />
+        </Col>
+        <Col>
+          <div className="navcontent"></div>
+        </Col>
       </Container>
     );
   }
--- src/components/products.js
+++ src/components/products.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
-import { Spinner } from "react-bootstrap";
+import React, { Component, useState } from "react";
+import { Col, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Container, Sidebar, Sidenav, Nav, Button } from "rsuite";
 import AddModal from "../modal/addModal";
+import Dashboard from "./dashboard";
 import AddProductForm from "./helpers/Forms/addProductForm";
 import ProductTable from "./helpers/Tables/ProductTable";
 import useFetchProducts from "./Hooks/useFetchProducts";
+import SideNavigation from "./sidenavigation";
 
 export default function Transactions() {
   const [show, setShow] = useState(false);
@@ -32,64 +34,35 @@ export default function Transactions() {
   );
   return (
     <Container className="container">
-      <div className="navbar">
-        <Sidebar className="sidenavigation">
-          <Sidenav>
-            <Sidenav.Header className="sidebarheader">
-              <h3 className="sidebarheaderh3">Welcome to</h3>
-              <h1 className="sidebarheaderh1">OWRKS</h1>
-            </Sidenav.Header>
-            <Sidenav.Body className="sidenavigationbody">
-              <Nav>
-                <Link to="/dashboard" className="navlink">
-                  <Nav.Item className="navitem">Dashboard</Nav.Item>
-                </Link>
-                <Link to="/users" className="navlink">
-                  <Nav.Item className="navitem">Users</Nav.Item>
-                </Link>
-                <Link to="/products" className="navlink">
-                  <Nav.Item className="navitem navitemactive">
-                    Products
-                  </Nav.Item>
-                </Link>
-                <Link to="/suppliers" className="navlink">
-                  <Nav.Item className="navitem">Suppliers</Nav.Item>
-                </Link>
-                <Link to="/transactions" className="navlink">
-                  <Nav.Item className="navitem">Transactions</Nav.Item>
-                </Link>
-                <Link to="/">
-                  <Button className="logoutbutton">Logout</Button>
-                </Link>
-              </Nav>
-            </Sidenav.Body>
-          </Sidenav>
-        </Sidebar>
-      </div>
-      <div className="navcontent">
-        <div class="container-fluid searchoptions">
-          <form class="d-flex">
-            <input
-              className="searchforminput"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <Button className="searchbutton">Search</Button>
-            <Button className="addbutton" onClick={() => setShow(true)}>
-              Add Product
-            </Button>
-            <AddModal
-              title="Add Product"
-              onClose={() => setShow(false)}
-              show={show}
-              // onSubmit={<Dashboard />}
-              button="Add Product"
-            >
-              <AddProductForm closeModal={closeModal} />
-            </AddModal>
-            <Button className="deletebutton">Delete Product</Button>
-          </form>
+      <Col md={3}>
+        <SideNavigation />
+      </Col>
+      <Col>
+        <div className="navcontent">
+          <div class="container-fluid searchoptions">
+            <form class="d-flex">
+              <input
+                className="form-control searchforminput"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <Button className="searchbutton">Search</Button>
+              <Button className="addbutton" onClick={() => setShow(true)}>
+                Add Product
+              </Button>
+              <AddModal
+                title="Add Product"
+                onClose={() => setShow(false)}
+                show={show}
+                // onSubmit={<Dashboard />}
+                button="Add Product"
+              >
+                <AddProductForm closeModal={closeModal} />
+              </AddModal>
+              <Button className="deletebutton">Delete Product</Button>
+            </form>
+          </div>
           <div>
             {!myloading ? (
               results
@@ -98,7 +71,7 @@ export default function Transactions() {
             )}
           </div>
         </div>
-      </div>
+      </Col>
     </Container>
   );
 }
